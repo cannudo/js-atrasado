a1a3325f442400298eaff5bc83910af95c0e1d3e
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-12-07 13:43:23 -0300

    📝pega disciplina e coloca na lista do html

--- src/js/exercicio-um.js
+++ src/js/exercicio-um.js
@@ -1,2 +1,9 @@
-const json = [{"id":567,"dsecricao":"RIA","ativa":true,"professor":{"matricula":"4567890","nome":"Joao carlos"},"alunos":["maria","sergio","luan"]},{"id":123,"dsecricao":"APOO","ativa":false,"professor":{"matricula":"26352","nome":"Alessndro Souza"},"alunos":["maria","sergio","luan"]}]
-disciplinas = document.getElementById('disciplinas')
+const json_data = [{"id":567,"dsecricao":"RIA","ativa":true,"professor":{"matricula":"4567890","nome":"Joao carlos"},"alunos":["maria","sergio","luan"]},{"id":123,"dsecricao":"APOO","ativa":false,"professor":{"matricula":"26352","nome":"Alessndro Souza"},"alunos":["maria","sergio","luan"]}]
+const disciplinas = document.getElementById('disciplinas')
+
+json_data.forEach((disciplina) => {
+    node = document.createElement("li")
+    text = document.createTextNode(disciplina.id)
+    node.appendChild(text)
+    disciplinas.appendChild(node)
+})
